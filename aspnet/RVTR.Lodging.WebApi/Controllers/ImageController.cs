53d4cf680d5bbeac0dd765ca7e46bc0dd64d9b1a
Harold Hopson <hdhopson88@yahoo.com>
2020-09-18 07:33:15 -0700

    updated controller with configuration

--- aspnet/RVTR.Lodging.WebApi/Controllers/ImageController.cs
+++ aspnet/RVTR.Lodging.WebApi/Controllers/ImageController.cs
@@ -4,8 +4,13 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Azure;
 using RVTR.Lodging.DataContext.Repositories;
 using RVTR.Lodging.ObjectModel.Models;
+using Azure.Storage.Blobs;
+using System;
+using Azure.Storage.Blobs.Models;
 
 namespace RVTR.Lodging.WebApi.Controllers
 { 
@@ -20,16 +25,19 @@ namespace RVTR.Lodging.WebApi.Controllers
     {
         private readonly ILogger<ImageController> _logger;
         private readonly UnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
 
         /// <summary>
         /// Constructor for the ImageController sets up logger and unitOfWork dependencies
         /// </summary>
         /// <param name="logger">The Logger</param>
         /// <param name="unitOfWork">The UnitOfWork</param>
-        public ImageController(ILogger<ImageController> logger, UnitOfWork unitOfWork)
+        /// <param name="configuration">The Configuration</param>
+        public ImageController(ILogger<ImageController> logger, UnitOfWork unitOfWork, IConfiguration configuration)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -40,7 +48,30 @@ namespace RVTR.Lodging.WebApi.Controllers
         [ProducesResponseType(typeof(IEnumerable<string>), StatusCodes.Status200OK)]
         public async Task<IActionResult> Get()
         {
-            return Ok(await _unitOfWork.Lodging.SelectAsync());
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_configuration.GetConnectionString("blob"));
+
+            // Get the container and return a container client object
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
+            
+            var images = new List<BlobItem>();
+
+            // List all blobs in the container
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                images.Add(blobItem);
+                Console.WriteLine("\t" + blobItem.Name);
+            }
+
+            // BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+            // using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+            // {
+            //     await download.Content.CopyToAsync(downloadFileStream);
+            //     downloadFileStream.Close();
+            // }
+
+            return Ok(images);
         }
 
     }
